d08ede8db58710d41c52bea6b42028cf558e7f72
HariKishore <harikishoretm@gmail.com>
2025-08-24 11:05:44 +0530

    update settings.py to use database_url for render

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -76,15 +76,26 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'movie_ticket',
-        'USER' : 'postgres',
-        'PASSWORD' : '12345',
-        'HOST' : 'localhost'
+import dj_database_url
+
+if os.getenv("DATABASE_URL"):
+    # Use Render/Production database
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ['DATABASE_URL'])
     }
-}
+else:
+    # Local fallback database (Postgres or SQLite)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'movie_ticket',
+            'USER': 'postgres',
+            'PASSWORD': '12345',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
+
 
 
 # Password validation
